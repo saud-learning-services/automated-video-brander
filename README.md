43853d2c5b514234a620806b2b41794a239aa027
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-07-15 09:04:04 -0700

    Added ability to include body and watermarking functionality

--- README.md
+++ README.md
@@ -1,37 +1,48 @@
 # Top and Tail Stitcher
 
 ## ⚠️ Important Use Case Information 
-* Editors will need to ensure that tops are created ~6s with no fadeout
-* Only tested with 1920x1080 (MP4) 
-* output directory will be cleared at the start of each run
+* Editors will need to ensure that tops are created ~6s (no fades)
+* Only works with **1920x1080 (MP4)**
+* `/output` directory will be cleared at the start of each run
+* Body should be a single video clip with ~3 seconds before/after the spaeker speaks
+* Watermark position defaults to bottom-right -- bottom left only if WatermarkPosition is **L** or **l** (in CSV)
 
 ## 📋 TODO
-* ~~get the Elicia Salzberg video for COMM 101~~
-* ~~Do fades to white programmatically on top assets (tail is constant)~~
-* Bring specifications table up to new standard
-* Get all video assets needed for new testing
-* Look into the Panopto API -- clarify with Altan, with every completed video we should be making a session?
+- [x] get the Elicia Salzberg video for COMM 101
+- [x] Do fades to white programmatically on top assets (tail is constant)
+- [x] Bring specifications table up to new standard
+- [x] Get all video assets needed for new testing
+- [x] Body needs a fade from white ALWAYS
+- [x] Fix Watermark - edges doing crazy shit
+- [x] Add ability to specify Watermark location
+- [] Make titles dynamic
+    - [] Create blank slide on Premiere Pro (make sure Audio fades out completely)
+    - [] Add text overlay and animation using moviepy
+- [] get it on GitHub and add everyone:
+    - [] shoekey4811
+    - [] unabridgedxcrpt
 
-## 🤔 Possible Features
-* Jamie mentioned something about an additional title slide? Like a course slide then a section slide?
-* Body addition (optional - if we already have the body we can add it to the edit)
-    * Watermark addition: Ability to specify which watermark to use and placement (bottom left vs bottom right)
-    * Programmatically cross-dissolve between top and tail?
-* Panopto integration and uploading (nice-to-have but could be time consuming)
+## Next Steps
+- [] Second script does the Panopto part of everything, first script just makes the video files
+- [] Integrate with Panopto to upload to sessions according to Altan and Jonathan's specifications
+- [] ERROR HANDLING
 
-## Notes
+## CSV
+Any of the following values will be treated as NULL on CSV:
 
-* tier1: prebuild courses (prior to content)
-* watermarking is going to be key (2 watermarks)
-* get it on GitHub and add everyone:
-    * shoekey4811
-    * unabridgedxcrpt
+* empty cell
+* empty string: '' (FALSY)
+* zero: 0 (FALSY)
+* type = nan OR type = None
+* 'null' OR 'NULL'
+* 'none' OR 'NONE'
 
-Does it have a body?
+## Whoever is runing should have a basic understanding of:
 
-Yes? Does it have a watermark?
+* How to run a python script via the command line (for whatever OS you're using)
+* How to setup and manage an environment using conda given a `environment.yml`
+* How to keep their version up-to-date using GitHub (or GitHub Desktop)
+    * Note you don't need to use GitHub but then you're responsible for keeping your version up to date manually
 
-Yes? Where does the watermark go?
+These are the same expectations when delivering a script to OPS. Marko will not be offering one-on-one support for anyting Anaconda/Conda related (Except for one session if there is interest)
 
-
-Seems like it's possible to build out folder hierarchies & make sessions using the Panopto API
--- stitch.py
+++ stitch.py
@@ -1,16 +1,35 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip, concatenate_videoclips
 import moviepy.video.fx.all as vfx
 from termcolor import cprint
 import pandas as pd
 import shutil
+import math
 import os
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 
 print('\n🚚 Loading Specifications from CSV...')
 specs = pd.read_csv(root_path + '/input/specifications.csv')
+def hasValue(cell):
+
+    # Falsy values are FALSE
+    # List of Falsy values in Python: https://stackoverflow.com/questions/39983695/what-is-truthy-and-falsy-how-is-it-different-from-true-and-false
+    if (not cell):
+        return False
+
+    # nan values FALSE
+    if (not isinstance(cell, str)):
+        return False
+
+    # strings == 'none' or 'null' or '0' are also FALSE
+    if (cell.lower() == 'none' or cell.lower() == 'null' or 
+        cell.lower() == 'nan' or cell == '0'):
+        return False
+    
+    return True
 
 # ===== DELETE OUTPUT CONTENTS =====
+
 def clear_output_contents():
     """
     Clears output directory contents
@@ -48,17 +67,55 @@ for index, row in specs.iterrows():
     course = row['Course']
     title = row['Title']
     top = row['Top']
-    \
-    print('\n==========')
+    body = row['Body']
+    watermark = row['Watermark']
+    wm_position = row['WatermarkPosition']
+
+    print('\n==========') 
     print(f'🎥 {course} | {title}')
 
-    top = VideoFileClip(f'input/tops/{top}')
-    top_with_fade = top.fx(vfx.fadeout, duration=1.5, final_color=[255,255,255])
+    ### SOME DEBUGGING PRINTOUTS ###
+    # print(f'body: {body} | has: {hasValue(body)}')
+    # print(f'watermark: {watermark} | has: {hasValue(watermark)}')
+    # print(f'position: {wm_position} | has: {hasValue(wm_position)}')
+    # ======== #
 
+    top = VideoFileClip(f'input/top/{top}')
+    top_with_fade = top.fx(vfx.fadeout, duration=1.5, final_color=[255,255,255])
+    
+    # same tail for all videos
     tail = VideoFileClip('input/tail/tail.mp4')
-    final_clip = concatenate_videoclips([top_with_fade, tail])
+
+    if (hasValue(body)):
+        raw_body = (VideoFileClip(f'input/body/{body}')
+                    .fx(vfx.fadein, duration=1, initial_color=[255, 255, 255]))
+        if (hasValue(watermark)):
+
+            ## DEFAULT (bottom right)
+            left_margin = 1575
+
+            ## bottom left if specified
+            if (wm_position == 'L' or wm_position == 'l'):
+                left_margin = 50
+
+            watermark = (ImageClip(f'input/watermark/{watermark}')
+                        .set_opacity(0.8)
+                        .set_duration(raw_body.duration)
+                        .resize(height=70)
+                        .margin(left=left_margin, top=975, opacity=0))
+
+            body_wm = CompositeVideoClip([raw_body, watermark])
+            body = body_wm.fx(vfx.fadeout, duration=1, final_color=[255,255,255])
+        else:
+            body = raw_body.fx(vfx.fadeout, duration=1, final_color=[255,255,255])
+        final_clip = concatenate_videoclips([top_with_fade, body, tail])
+    else:
+        final_clip = concatenate_videoclips([top_with_fade, tail])
+
 
     print(f'📁 Writing to output folder {course}...')
     final_clip.write_videofile(f'output/{course}/{title}.mp4', temp_audiofile='temp-audio.m4a', remove_temp=True, codec='libx264', audio_codec='aac')
 
+
+    # TODO: need to do some checking here
     cprint('\nSUCCESS', 'green')
